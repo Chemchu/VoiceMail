355ba513d645943fc86a02fecdb35b4f53ff8fcf
gus <gustavo_9_rj@hotmail.com>
2022-04-24 01:43:18 +0200

    Elementos visuales añadidos

--- src/components/logoHeader/index.tsx
+++ src/components/logoHeader/index.tsx
@@ -1,16 +1,23 @@
 const LogoHeader = () => {
     return (
-        <>
-            <span style={{ color: "#3e65cf" }}>V</span>
-            <span style={{ color: "#c71610" }}>o</span>
-            <span style={{ color: "#f2a60c" }}>i</span>
-            <span style={{ color: "#08851b" }}>c</span>
-            <span style={{ color: "#3e65cf" }}>e</span>
-            <span>M</span>
-            <span>a</span>
-            <span>i</span>
-            <span>l</span>
-        </>
+        <div className="flex gap-4 items-end">
+            <div>
+                <span style={{ color: "#3e65cf" }}>V</span>
+                <span style={{ color: "#c71610" }}>o</span>
+                <span style={{ color: "#f2a60c" }}>i</span>
+                <span style={{ color: "#08851b" }}>c</span>
+                <span style={{ color: "#3e65cf" }}>e</span>
+                <span>M</span>
+                <span>a</span>
+                <span>i</span>
+                <span>l</span>
+            </div>
+            <div style={{ color: "#3e65cf" }}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                </svg>
+            </div>
+        </div>
     );
 }
 
--- src/components/speakBar/index.tsx
+++ src/components/speakBar/index.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { useEffect } from "react";
 import useSpeechToText from "react-hook-speech-to-text";
 
@@ -52,14 +53,15 @@ const SpeakBar = (props: { setPhrase: React.Dispatch<React.SetStateAction<string
                     }
                     {interimResult && <li>{interimResult}</li>}
                 </span>
-                <div className={`text-gray-400 hover:cursor-pointer ${isRecording ? 'animate-bounce text-red-500' : 'hover:text-blue-500'}`}
+                <motion.div whileHover={{ scale: 1.1 }}
+                    className={`text-gray-400 hover:cursor-pointer ${isRecording ? 'animate-bounce text-red-500' : 'hover:text-blue-500'}`}
                     onClick={isRecording ? stopSpeechToText : startSpeechToText}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z" />
                     </svg>
-                </div>
+                </motion.div>
             </div>
-        </div>
+        </div >
     )
 }
 
